7c1806aa9d139d36e078dc6fd22a7d9d3ab68d1f
zhangtao-code <65645107+zhangtao-code@users.noreply.github.com>
2021-11-18 15:00:54 +0800

    数组队列实现

--- src/cacpter1_3/common/ArrayQueue.java
+++ src/cacpter1_3/common/ArrayQueue.java
@@ -5,8 +5,9 @@ import java.util.Iterator;
 public class ArrayQueue<T> implements Queue<T>{
     protected Object[]array;
     protected int factor;
-    protected int size;
+    protected int read;
     protected int write;
+    protected int size;
     public ArrayQueue(int cap,int factor) {
         this.factor = factor;
         array=new Object[cap];
@@ -17,31 +18,68 @@ public class ArrayQueue<T> implements Queue<T>{
 
     @Override
     public void enqueue(T t) {
-
+        if(size>=array.length){
+            resize();
+        }
+        size++;
+        array[write]=t;
+        write=(write+1)%array.length;
     }
 
     @Override
     public T dequeue() {
-        return null;
-
+        if(size<=0){
+            return null;
+        }
+        T t=(T)array[read];
+        array[read]=null;
+        read=(read+1)%array.length;
+        size--;
+        return t;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size==0;
     }
+
+
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public Iterator<T> iterator() {
-        return null;
+        Object[]clone=new Object[size];
+        int current=size;
+        int rIndex=read;
+        int index=0;
+        while (current>0){
+            clone[index++]=array[rIndex];
+            rIndex=(rIndex+1)%array.length;
+            current--;
+        }
+        return new IteratorArray<>(clone);
     }
     protected void resize(){
-
+        int length=array.length*(1+factor);
+        if(length==size){
+            length*=2;
+        }
+        Object[]clone=new Object[length];
+        int current=size;
+        int rIndex=read;
+        int index=0;
+        while (current>0){
+            clone[index++]=array[rIndex];
+            rIndex=(rIndex+1)%array.length;
+            current--;
+        }
+        array=clone;
+        read=0;
+        write=size;
     }
 
 }
